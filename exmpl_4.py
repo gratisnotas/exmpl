4b707c1a982d33a49dfdd155708d516cfd431168
gratisnotas <158621169+gratisnotas@users.noreply.github.com>
2024-12-21 00:45:17 +0530

    modified exmpl 4
    Included numpy array for faster plotting

--- exmpl_4.py
+++ exmpl_4.py
@@ -3,13 +3,12 @@
 # multiple plots
 
 import matplotlib.pyplot as plt
+from numpy import array as ar
 
-X = range(1,5)
-Y1 = [x**2 for x in X]
-Y2 = [x*3 for x in X]
-Y3 = [x*4 for x in X]
-
-print(Y3)
+X = ar(range(1,5))
+Y1 = X**2
+Y2 = X*3
+Y3 = X*4
 
 plt.plot(X, Y1, X, Y2, X, Y3) # same X different Y
 plt.show()
