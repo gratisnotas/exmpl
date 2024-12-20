dbb8411d1dede0fe9cb387a09174f580be657330
gratisnotas <158621169+gratisnotas@users.noreply.github.com>
2024-12-21 00:04:44 +0530

    Added exmpl 1
    a simple plot without x axis

---
+++ exmpl_1.py
@@ -0,0 +1,8 @@
+# exmpl 1
+
+# a simple plot to get started
+import matplotlib.pyplot as plt
+plt.plot([1,3,2,4])
+plt.show()
+
+# since x axis is not plotted, matplotlib assumes 0 to n-1 as x axis points
