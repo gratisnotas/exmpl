c786b87407e8f743df3b1c1ff39be100a9364693
gratisnotas <158621169+gratisnotas@users.noreply.github.com>
2024-12-21 17:47:08 +0530

    Added exmp 8
    title of plot along with x- and y-labels, and grid

---
+++ exmpl_8.py
@@ -0,0 +1,14 @@
+# exmpl 8
+
+# adding title to a plot
+
+import matplotlib.pyplot as plt
+
+plt.title("Title of Plot")
+
+plt.xlabel("x values")
+plt.ylabel("y values")
+plt.grid(True)
+
+plt.plot([1,3,2,4])
+plt.show()
